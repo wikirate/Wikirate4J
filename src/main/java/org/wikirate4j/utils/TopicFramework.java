b6b9c7114410f94b6d07e5038f714ea13dc1b2f8
vasgat <vasgat@gmail.com>
2025-12-18 11:16:36 +0100

    feat(WDM-16): add Topic and TopicFramework enums to align with platform updates and API structure. These enums provide a type-safe way to reference topics and topic frameworks and will be used accross queries.

---
+++ src/main/java/org/wikirate4j/utils/TopicFramework.java
@@ -0,0 +1,33 @@
+package org.wikirate4j.utils;
+
+/**
+ * @author Vasiliki Gkatziaki
+ */
+public enum TopicFramework {
+
+    WIKIRATE_ESG_TOPICS("Wikirate_ESG_Topics"),
+    GRI_STANDARDS("GRI_Standards"),
+    ESRS_STANDARDS("ESRS_Standards"),
+    UN_SDGs("UN_SDGs"),
+    UNKNOWN("");
+
+    private final String value;
+
+    private TopicFramework(String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return this.value;
+    }
+
+    public static TopicFramework getTopicFramework(String topic_framework) {
+        for (TopicFramework topicFramework : values()) {
+            if (topicFramework.value().equals(topic_framework)) {
+                return topicFramework;
+            }
+        }
+
+        return UNKNOWN;
+    }
+}
